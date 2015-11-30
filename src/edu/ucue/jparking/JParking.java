cb755df58f7f52d221f10e14261ed5f735b05673
Santos Gallegos <santos_g@outlook.com>
2015-11-30 17:21:38 -0500

    Primer commit
    Establecido descripcion general del proyecto y autores del mismo.

---
+++ src/edu/ucue/jparking/JParking.java
@@ -0,0 +1,23 @@
+/*
+ * Administrador de los parqueaderos ubicados en la Universidad de Cuenca
+ * Autores del proyecto: 
+ * Santos Gallegos <santos_g@outlook.com>
+ * Franklin Lara <>
+ *
+ */
+package edu.ucue.jparking;
+
+/**
+ *
+ * @author Santos Gallegos, Franklin Lara
+ */
+public class JParking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
