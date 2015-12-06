4df7690aaf799c59386bacaac94e540f382d6590
Santos Gallegos <santos_g@outlook.com>
2015-12-05 23:01:36 -0500

    Agregada docuementacion del proyecto

--- src/edu/ucue/jparking/JParking.java
+++ src/edu/ucue/jparking/JParking.java
@@ -1,9 +1,28 @@
-/*
+/**
  * Administrador de los parqueaderos ubicados en la Universidad de Cuenca
- * Autores del proyecto: 
- * Santos Gallegos <santos_g@outlook.com>
- * Franklin Lara <larafranklin@outlook.com>
+ * 
+ * @author Santos Gallegos <santos_g@outlook.com>
+ * @author  Franklin Lara <larafranklin@outlook.com> 
+ *  
+ * @version 0.0.1
  *
+ * Descripcion del programa:
+ * Permite al adminstrador crear campus, parqueaderos dentro de estos.
+ * Dentro de un campus se dentrá puertas de ingreso y/o salida hacia los
+ * paqrqueaderos.
+ * Un paqueadero puede tener varias puertas de acceso y de salida.
+ * Dentro de cada parqueadero se contará con puestos de parqueo.
+ * El número de usuarios de un parqueadero puede ser como máximo el 
+ * 150% de los puestos de parqueo disponibles en el parqueadero.
+ * El administrador podrá administrar los campus, parqueaderos, usuarios,
+ * puertas, puestos de parqueo, porteros.
+ * Los tipos de usuario existentes en el sistema son:
+ *      Portero: quien tiene acceso a todas las puertas pertenecientes al
+ *      campus donde pertenece.
+ *      Docente, empleado, estudiante: Sólo tienen acceso a las puertas
+ *      registradas de acceso/salida correspondientes al parqueadero donde
+ *      pertece cada usuario.
+ * 
  */
 package edu.ucue.jparking;
 
