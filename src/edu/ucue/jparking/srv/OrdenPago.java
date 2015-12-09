ad3c94407090d04b6fcc8a889d86c25c78657a2a
Franklin <Franklin@172.16.147.206>
2015-12-09 14:58:19 -0500

    clases orden de pago modificada

--- src/edu/ucue/jparking/srv/OrdenPago.java
+++ src/edu/ucue/jparking/srv/OrdenPago.java
@@ -5,11 +5,38 @@
  */
 package edu.ucue.jparking.srv;
 
+import java.util.Calendar;
+
 /**
  *
  * @author stsewd
  */
 public class OrdenPago {
+    
+    private String cadulaUsuario;
+    private float costo;
+    private Calendar fechaEmision;
+
+    public OrdenPago(String cadulaUsuario, float costo) {
+        this.cadulaUsuario = cadulaUsuario;
+        this.costo = costo;
+        this.fechaEmision = Calendar.getInstance();
+    }
+
+    
+    public void setCadulaUsuario(String cadulaUsuario) {
+        this.cadulaUsuario = cadulaUsuario;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+
+    public void setFechaEmision(Calendar fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+    
+    
     
     
 }
