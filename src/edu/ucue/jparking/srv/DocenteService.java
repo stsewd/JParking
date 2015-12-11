80cc797b6776eef83613a72cc6526d78df24e9e3
ESTUDIANTE <ESTUDIANTE@172.16.147.153>
2015-12-11 18:40:26 -0500

    Estudiante service Docente service Empleado service

---
+++ src/edu/ucue/jparking/srv/DocenteService.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ucue.jparking.srv;
+
+import edu.ucue.jparking.dao.UsuarioNoExistenteException;
+import edu.ucue.jparking.dao.UsuarioYaExistenteException;
+import edu.ucue.jparking.dao.UsuariosDAO;
+import java.util.Set;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class DocenteService {
+    
+    Validaciones validar = new Validaciones();
+    
+    
+    
+    public void add(String cedula, String nombre, String apellido) throws UsuarioYaExistenteException, CedulaNoValidaException {
+        
+        validar.ValidarDatos(cedula, nombre, apellido);
+        validar.validarCedula(cedula);
+        Docente docente = new Docente(cedula, nombre, apellido);
+        UsuariosDAO.getInstance().addUsuario(docente);
+           
+    }
+
+    
+    public void del(String cedula) throws UsuarioNoExistenteException, CedulaNoValidaException {
+        validar.validarCedula(cedula);
+        UsuariosDAO.getInstance().delUsuario(cedula);
+    }
+
+    
+    public Docente get(String cedula) throws UsuarioNoExistenteException, CedulaNoValidaException {
+        validar.validarCedula(cedula);
+        return (Docente) UsuariosDAO.getInstance().getUsuario(cedula);
+    }
+        
+    public void mod(String cedula, String nombre, String apellido,boolean estado) throws CedulaNoValidaException, UsuarioNoExistenteException{
+        validar.validarCedula(cedula);
+        validar.ValidarDatos(cedula, nombre, apellido);
+        UsuariosDAO.getInstance().modUsuario(cedula, nombre, apellido, estado);
+        
+        
+    }
+
+    
+    public Set getLista() {
+        return (Set) UsuariosDAO.getInstance().getUsuarios();
+    }
+}
--- src/edu/ucue/jparking/srv/UsuarioService.java
+++ src/edu/ucue/jparking/srv/UsuarioService.java
@@ -5,15 +5,64 @@
  */
 package edu.ucue.jparking.srv;
 
+import edu.ucue.jparking.dao.UsuarioNoExistenteException;
 import edu.ucue.jparking.dao.UsuarioYaExistenteException;
 import edu.ucue.jparking.dao.UsuariosDAO;
+import java.util.Set;
 
 /**
  *
  * @author Franklin Lara
  */
-public interface UsuarioService {
+public class UsuarioService {
     
+    public void add(String cedula, String nombre, String apellido, TipoUsuario tipoUsuario) throws UsuarioYaExistenteException, CedulaNoValidaException{
+        if(tipoUsuario==TipoUsuario.ESTUDIANTE){
+            EstudianteService estudianteService = new EstudianteService();
+            estudianteService.add(cedula, nombre, apellido);
+        }else if(tipoUsuario==TipoUsuario.DOCENTE){
+            DocenteService docenteService = new DocenteService();
+            docenteService.add(cedula, nombre, apellido);
+        }else if(tipoUsuario==TipoUsuario.EMPLEADO){
+            EmpleadoService empleadoService = new EmpleadoService();
+            empleadoService.add(cedula, nombre, apellido);
+        }else{
+            throw new IllegalArgumentException("El argumento tipo usuario no puede estar vacio");
+        } 
+    }
     
+    public void del(String cedula,TipoUsuario tipoUsuario) throws UsuarioNoExistenteException, CedulaNoValidaException{
+        if(tipoUsuario==TipoUsuario.ESTUDIANTE){
+            EstudianteService estudianteService = new EstudianteService();
+            estudianteService.del(cedula);
+        }else if(tipoUsuario==TipoUsuario.DOCENTE){
+            DocenteService docenteService = new DocenteService();
+            docenteService.del(cedula);
+        }else if(tipoUsuario==TipoUsuario.EMPLEADO){
+            EmpleadoService empleadoService = new EmpleadoService();
+            empleadoService.del(cedula);
+        }else{
+            throw new IllegalArgumentException("El argumento tipo usuario no puede estar vacio");
+        } 
+    }
+    
+    public void mod(String cedula, String nombre, String apellido,boolean estado, TipoUsuario tipoUsuario) throws CedulaNoValidaException, UsuarioNoExistenteException {
+        if(tipoUsuario==TipoUsuario.ESTUDIANTE){
+            EstudianteService estudianteService = new EstudianteService();
+            estudianteService.mod(cedula, nombre, apellido, estado);
+        }else if(tipoUsuario==TipoUsuario.DOCENTE){
+            DocenteService docenteService = new DocenteService();
+            docenteService.mod(cedula, nombre, apellido, estado);
+        }else if(tipoUsuario==TipoUsuario.EMPLEADO){
+            EmpleadoService empleadoService = new EmpleadoService();
+            empleadoService.mod(cedula, nombre, apellido, estado);
+        }else{
+            throw new IllegalArgumentException("El argumento tipo usuario no puede estar vacio");
+        } 
+    }
+    
+    public Set getLista(){
+        
+    }
    
 }
