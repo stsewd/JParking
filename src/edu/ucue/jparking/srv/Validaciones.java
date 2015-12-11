19932ee2e0fc1a1de0665c2926fb284f3790ecd6
ESTUDIANTE <ESTUDIANTE@172.16.147.153>
2015-12-11 18:17:34 -0500

    Estudiante service

--- src/edu/ucue/jparking/srv/Validaciones.java
+++ src/edu/ucue/jparking/srv/Validaciones.java
@@ -7,57 +7,46 @@ package edu.ucue.jparking.srv;
 
 /**
  *
- * @author ESTUDIANTE
+ * @author Franklin Lara
  */
 public class Validaciones {
     //private String aux;
-    public boolean ValidarCedula(String cedula){
-        
-        cedula=cedula.trim();
-        for (int i = 0; i < cedula.length(); i++) {
-            if (!Character.isDigit(cedula.charAt(i)))
-                return false;
-        }
-        
-        return true;
-    }
-    ///funcion que hace la validacion de la cedula hacer modificaciones
-/*
-public boolean validadorDeCedula(String cedula) {
+
+public boolean validarCedula(String cedula) throws CedulaNoValidaException {
     boolean cedulaCorrecta = false;
 
     try {
 
-    if (cedula.length() == 10) // ConstantesApp.LongitudCedula
-    {
-        int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
-        if (tercerDigito < 6) {
-            // Coeficientes de validación cédula
-            // El decimo digito se lo considera dígito verificador
-             int[] coefValCedula = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
-             int verificador = Integer.parseInt(cedula.substring(9,10));
-             int suma = 0;
-             int digito = 0;
-            for (int i = 0; i < (cedula.length() - 1); i++) {
-             digito = Integer.parseInt(cedula.substring(i, i + 1))* coefValCedula[i];
-             suma += ((digito % 10) + (digito / 10));
-            }
+        if (cedula.length() == 10) // ConstantesApp.LongitudCedula
+        {
+            int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
+            if (tercerDigito < 6) {
+                // Coeficientes de validación cédula
+                // El decimo digito se lo considera dígito verificador
+                 int[] coefValCedula = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+                 int verificador = Integer.parseInt(cedula.substring(9,10));
+                 int suma = 0;
+                 int digito = 0;
+                for (int i = 0; i < (cedula.length() - 1); i++) {
+                 digito = Integer.parseInt(cedula.substring(i, i + 1))* coefValCedula[i];
+                 suma += ((digito % 10) + (digito / 10));
+                }
 
-            if ((suma % 10 == 0) && (suma % 10 == verificador)) {
-                cedulaCorrecta = true;
-            }
-            else if ((10 - (suma % 10)) == verificador) {
-                cedulaCorrecta = true;
+                if ((suma % 10 == 0) && (suma % 10 == verificador)) {
+                    cedulaCorrecta = true;
+                }
+                else if ((10 - (suma % 10)) == verificador) {
+                    cedulaCorrecta = true;
+                } else {
+                    cedulaCorrecta = false;
+                }
             } else {
                 cedulaCorrecta = false;
             }
         } else {
-        cedulaCorrecta = false;
-        }
-        } else {
             cedulaCorrecta = false;
         }
-        
+
     } catch (NumberFormatException nfe) {
             
         cedulaCorrecta = false;
@@ -71,9 +60,21 @@ public boolean validadorDeCedula(String cedula) {
     }
 
     if (!cedulaCorrecta) {
-    System.out.println("La Cédula ingresada es Incorrecta");
+        throw new CedulaNoValidaException(cedula);
     }
     return cedulaCorrecta;
     }
-    */
+    
+
+public void ValidarDatos(String cedula,String nombre, String apellidos)
+    {
+        if(cedula==null || cedula.trim().length()==0)
+            throw new IllegalArgumentException("El campo cedula no puede estar vacio");
+        if(nombre==null || nombre.trim().length()==0)
+            throw new IllegalArgumentException("El argumento nombre no puede estar vacio");
+        if(apellidos==null || apellidos.trim().length()==0)
+            throw new IllegalArgumentException("El argumento apellidos no puede estar vacio");
+        
+    }
+
 }
