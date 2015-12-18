926e94ed465b0511a8c9489974f436f3e13972f6
Santos Gallegos <santos_g@outlook.com>
2015-12-18 15:57:04 -0500

    Interface DAO parqueaderos

--- src/edu/ucue/jparking/dao/interfaces/ParqueaderosDAOInterface.java
+++ src/edu/ucue/jparking/dao/interfaces/ParqueaderosDAOInterface.java
@@ -6,7 +6,6 @@
 package edu.ucue.jparking.dao.interfaces;
 
 import edu.ucue.jparking.srv.objetos.Parqueadero;
-import edu.ucue.jparking.srv.objetos.Puerta;
 import java.util.Set;
 
 /**
@@ -14,13 +13,58 @@ import java.util.Set;
  * @author stsewd
  */
 public interface ParqueaderosDAOInterface {
-    //NO USAR AÚNNNN!!!!!
-    public void addParqueadero();
-    public void delParqueadero();
-    public Parqueadero getParqueadero();
-    public Set<Parqueadero> getParqueaderos();
-    public Set<Parqueadero> getParqueaderos(String nombreCampus);
-    public void modParqueadero();
-    public void addPuertaEntrada(String idCampus, String idPuerta);
+    /**
+     * Agrega un nuevo parqueadero al campus dado.
+     * @param nombreCampus
+     * @param parqueadero 
+     */
+    public void addParqueadero(String nombreCampus, Parqueadero parqueadero);
     
+    /**
+     * Elimina un parqueadero dado su id
+     * @param idParqueadero 
+     */
+    public void delParqueadero(String idParqueadero);
+    
+    /**
+     * Obtiene un parqueadero dado su id
+     * @param idParqueadero
+     * @return Parqueadero
+     */
+    public Parqueadero getParqueadero(String idParqueadero);
+    
+    /**
+     * Retorna un set con todos los parqueaderos registrados
+     * @return Set de todos los parqueaderos
+     */
+    public Set<Parqueadero> getParqueaderos();
+    
+    /**
+     * Retorna todos los parqueadero de un campus dado.
+     * @param nombreCampus
+     * @return Set de todos los parqueaderos de un campus
+     */
+    public Set<Parqueadero> getParqueaderos(String nombreCampus);
+    
+    /**
+     * Modifica los campos: ubicacion de un parqueadero dado su id.
+     * @param idParqueadero
+     * @param ubicacion 
+     */
+    public void modParqueadero(String idParqueadero, String ubicacion);
+    
+    /**
+     * Agrega una puerta de entrada a un parqueadero dado su id.
+     * @param idParqueadero
+     * @param idPuerta 
+     */
+    public void addPuertaEntrada(String idParqueadero, String idPuerta);
+    
+    
+    /**
+     * Agrega una puerta de salida a un parqueadero dado su id.
+     * @param idParqueadero
+     * @param idPuerta 
+     */
+    public void addPuertaSalida(String idParqueadero, String idPuerta);
 }
