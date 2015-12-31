ba9c9db2d43195b5cb3a3dd44818229b09792c47
Santos Gallegos <santos_g@outlook.com>
2015-12-31 09:47:44 -0500

    Ocultado boton autenticar de adm puertas

--- src/edu/ucue/jparking/gui/AutenticarGUI.java
+++ src/edu/ucue/jparking/gui/AutenticarGUI.java
@@ -182,7 +182,7 @@ public class AutenticarGUI extends javax.swing.JDialog {
         } catch (IllegalArgumentException | CedulaNoValidaException | UsuarioNoExistenteException | CodigoNoValidoException | ParqueaderoNoExistenteException | AccesoNoAutorizadoException | CampusNoExistenteException ex) {
             JOptionPane.showMessageDialog(rootPane, ex.getMessage(), "Error", JOptionPane.OK_OPTION);
         } catch (Exception ex){
-            JOptionPane.showMessageDialog(rootPane, "Algo inesperado paso", "Mensaje", JOptionPane.OK_OPTION);
+            JOptionPane.showMessageDialog(rootPane, "Algo inesperado pasó.", "Mensaje", JOptionPane.OK_OPTION);
         }
     }//GEN-LAST:event_AutenticarBtnActionPerformed
 
