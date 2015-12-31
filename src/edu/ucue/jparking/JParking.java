8593df6fa6de6a6251c68ccde4141bbaa6a940fb
Santos Gallegos <santos_g@outlook.com>
2015-12-31 00:22:04 -0500

    Update version

--- src/edu/ucue/jparking/JParking.java
+++ src/edu/ucue/jparking/JParking.java
@@ -4,7 +4,7 @@
  * @author Santos Gallegos <santos_g@outlook.com>
  * @author  Franklin Lara <larafranklin@outlook.com> 
  *  
- * @version 0.0.8-alfa
+ * @version 0.0.9-beta
  *
  * Descripcion del programa:
  * Permite al adminstrador crear campus, parqueaderos dentro de estos.
