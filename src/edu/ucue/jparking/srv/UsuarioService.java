01a3d8e03698ce4e8a32f5acc467bd63a4e37a97
Santos Gallegos <santos_g@outlook.com>
2015-12-30 23:30:10 -0500

    Registros de eliminacion/modificacion/acceso de usuarios

--- src/edu/ucue/jparking/srv/UsuarioService.java
+++ src/edu/ucue/jparking/srv/UsuarioService.java
@@ -14,6 +14,7 @@ import edu.ucue.jparking.dao.excepciones.CampusNoExistenteException;
 import edu.ucue.jparking.dao.excepciones.ParqueaderoNoExistenteException;
 import edu.ucue.jparking.dao.excepciones.PersonaYaRegistradoComoPorteroException;
 import edu.ucue.jparking.dao.interfaces.UsuariosDAOInterface;
+import edu.ucue.jparking.srv.enums.TipoAcceso;
 import edu.ucue.jparking.srv.enums.TipoModificacion;
 import edu.ucue.jparking.srv.enums.TipoUsuario;
 import edu.ucue.jparking.srv.excepciones.CodigoNoValidoException;
@@ -46,6 +47,7 @@ public class UsuarioService {
         }else{
             throw new IllegalArgumentException("El argumento tipo usuario no puede estar vacio");
         }
+        
         //Registro
         registroService.add(get(cedula).getRegistro(TipoModificacion.CREACION));
     }
@@ -54,12 +56,19 @@ public class UsuarioService {
         if(cedula == null || cedula.trim().length() == 0)
             throw new IllegalArgumentException("El argumendo cedula no puede ser vacio.");
         validaciones.validarCedula(cedula);
-        usuariosDAO.delUsuario(cedula);
+        
+        //Registro
+        registroService.add(get(cedula).getRegistro(TipoModificacion.ELIMINACION));
+        //Fin Registro
+        
+        usuariosDAO.delUsuario(cedula);        
     }
     
     public void mod(String cedula, String nombre, String apellido,String direccion,String telefono,boolean estado) throws CedulaNoValidaException, UsuarioNoExistenteException {
         validaciones.validarCedula(cedula);
         usuariosDAO.modUsuario(cedula, nombre, apellido, direccion, telefono, estado);
+        //Registro
+        registroService.add(get(cedula).getRegistro(TipoModificacion.MODIFICACION));
     }
     
     public Usuario get(String cedula) throws UsuarioNoExistenteException, CedulaNoValidaException{
@@ -98,6 +107,9 @@ public class UsuarioService {
             //Permitir acceso
             if(portero.getCampus().compareToIgnoreCase(nombreCampus) != 0)
                 throw new AccesoNoAutorizadoException(cedula, portero.getTipoUsuarioString(), nombreCampus, idPuerta);
+            //Registro
+            registroService.add(portero.getRegistro(TipoAcceso.ACCESO));
+            //Fin registro
             return;
         }
         
@@ -131,6 +143,12 @@ public class UsuarioService {
         
         if(!encontrado)
             throw new AccesoNoAutorizadoException(cedula, u.getTipoUsuarioString(), nombreCampus, idPuerta);
+        //Registro
+        registroService.add(u.getRegistro(TipoAcceso.ACCESO));
+        
+        /*************************************
+         * Implementar luego si entra o sale
+         *************************************/
     }
   
 }
